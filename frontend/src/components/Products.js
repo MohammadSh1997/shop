d694039450530f2d149978e0d2833e4660ac9543
MohammadSh1997 <MohammadSh1997ss@github.com>
2020-09-01 00:49:44 +0300

    add products

---
+++ frontend/src/components/Products.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Products(props) {
+    return (
+        <div className="products-items">
+            {props.products.map(product => {
+                return (
+                    <div key={product._id} className="item-card">
+                        <img src={product.image} alt="product" />
+                        <p className="product-title">{product.title}</p>
+                        <div className="product-data">
+                            <p className="product-price">${product.price}</p>
+                            <button className="product-button" > Add to cart </button>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Products
