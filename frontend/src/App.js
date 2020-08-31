43aa6253f9011cc99761e49801e2c3f3dd0046d0
MohammadSh1997 <MohammadSh1997ss@github.com>
2020-08-31 05:34:56 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h2>React Card Shopping</h2>
+        <ul className="navbar">
+          <li>Home</li>
+        </ul>
+      </header>
+      <main className="main">
+        <div className="content">
+          <div className="products">this is products</div>
+          <div className="cart">this is cart</div>
+        </div>
+      </main>
+      <footer>This is footer</footer>
+    </div>
+  );
+}
+
+export default App;
